fd75edc443ce99ece36962fc224c02b00483a2ec
Alexey Sazykin <916893+axin@users.noreply.github.com>
2022-10-01 17:09:13 +0000

    set up Typescript

--- packages/itgram-bot/src/logger.ts
+++ packages/itgram-bot/src/logger.ts
@@ -1,7 +1,7 @@
-import pino from "pino";
+import { pino, Logger } from "pino";
 
 export const logger = pino({
     level: process.env.ITGRAM_LOGLEVEL || "info",
 });
 
-export { Logger } from "pino";
+export { Logger };
